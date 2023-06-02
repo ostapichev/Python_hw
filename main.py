3ae1ebb9ca14a130befa9beb839e70674bfffb1d
Ostapichev <ytox@ukr.net>
2023-06-02 11:26:21 +0300

    HomeWork Python lesson 1

--- main.py
+++ main.py
@@ -5,8 +5,7 @@
 наприклад: st = 'as 23 fdfdg544' введена строка 2,3,5,4,4 вивело в консолі.
 """
 st = 'as 23 fdfdg544'
-list_num = [int(i) for i in st if i.isdigit()]
-print(list_num)
+print(', '.join(i for i in st if i.isdigit()))
 
 """
 2) написати прогу яка вибирає зі введеної строки числа і виводить їх 
@@ -14,19 +13,7 @@ print(list_num)
 наприклад: st = 'as 23 fdfdg544 34' #введена строка 23, 544, 34 вивело в консолі
 """
 st2 = 'as 23 fdfdg544 34'
-list_num2 = []
-str_num = ''
-for i in st2:
-    if i.isdigit():
-        str_num += i
-    elif str_num:
-        list_num2.append(str_num)
-        str_num = ''
-if str_num:
-    list_num2.append(str_num)
-
-list_result = [int(i) for i in list_num2]
-print(list_result)
+print(', '.join(''.join(ch if ch.isdigit() else ' ' for ch in st2).split()))
 
 # list comprehension
 """
@@ -36,25 +23,22 @@ greeting = 'Hello, world'
 ['H', 'E', 'L', 'L', 'O', ',', ' ', 'W', 'O', 'R', 'L', 'D']
 """
 greeting = 'Hello, world'
-list_greeting = [s.upper() for s in greeting]
-print(list_greeting)
+print([s.upper() for s in greeting])
 
 """
 2) з диапозону від 0-50 записати тільки не парні числа при цьому піднести їх до квадрату
 приклад:
 [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, ...]
 """
-list_num3 = [i ** 2 for i in range(51) if i % 2]
-print(list_num3)
+print([i ** 2 for i in range(51) if i % 2])
 
 
 # - створити функцію яка виводить ліст
-def get_list(*args):
-    return list(args)
+def get_list(*arr):
+    print(arr)
 
 
-array = get_list(1, 2, 3, 5, 8)
-print(array)
+get_list(1, 2, 3, 5, 8)
 
 
 # - створити функцію яка приймає три числа та виводить та повертає найбільше.
@@ -90,7 +74,6 @@ def get_min_list(nums):
     return min(nums)
 
 
-number = [3, 6, 10, 34, 8, 21]
 min_list = get_min_list(number)
 print(min_list)
 
@@ -100,7 +83,6 @@ def get_num_list(nums):
     return sum(nums)
 
 
-number = [3, 6, 10, 34, 8, 21]
 sum_list = get_num_list(number)
 print(sum_list)
 
@@ -110,7 +92,6 @@ def get_avg_list(nums):
     return sum(nums) // len(nums)
 
 
-number = [3, 6, 10, 34, 8, 21]
 avg_list = get_avg_list(number)
 print(avg_list)
 
@@ -166,8 +147,6 @@ print_square(10, 6, 8)
 """
 3) вывести табличку множення за допомогою цикла while
 """
-
-
 count = 0
 while count < 9:
     count += 1
